26220deb5f55afe099c96e19f2b74ed0e31e0584
mghafari3 <ghafarimustafa@yahoo.com>
2023-02-07 17:03:34 -0500

    using state (setState)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("Dev");
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <div>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
           <Employee name="Moose" role="Intern" />
-          <Employee name="Josh" role="Dev" />
+          <Employee name="Josh" role={role} />
           <Employee name="Brit" />
         </div>
       ) : (
