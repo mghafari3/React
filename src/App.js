c26780b721c23a39816179a98c6f49cfbbef9641
mghafari3 <ghafarimustafa@yahoo.com>
2023-02-08 18:04:16 -0500

    add tailwind layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("Dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Moose",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/14350433/pexels-photo-14350433.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      name: "Juice",
+      role: "Intern",
+      img: "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+    {
+      name: "Loose",
+      role: "",
+      img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+    {
+      name: "Noose",
+      role: "Prisoner",
+      img: "https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+    {
+      name: "Cus",
+      role: "Chief",
+      img: "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+    {
+      name: "Douce",
+      role: "Basketball Player",
+      img: "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?auto=compress&cs=tinysrgb&w=300",
+    },
+  ]);
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ? (
         <div>
           <input
@@ -15,9 +48,18 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <Employee name="Moose" role="Intern" />
-          <Employee name="Josh" role={role} />
-          <Employee name="Brit" />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
         </div>
       ) : (
         <p>You cannot see the employees</p>
