e7ab7e102633a4d2ec2d06de68f3bc0c6897296b
mghafari3 <ghafarimustafa@yahoo.com>
2023-02-09 16:58:29 -0500

    add update name and role func. to modal

--- src/App.js
+++ src/App.js
@@ -7,36 +7,55 @@ function App() {
   const [role, setRole] = useState("Dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Moose",
       role: "Developer",
       img: "https://images.pexels.com/photos/14350433/pexels-photo-14350433.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
+      id: 2,
       name: "Juice",
       role: "Intern",
       img: "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=300",
     },
     {
+      id: 3,
       name: "Loose",
       role: "",
       img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=300",
     },
     {
+      id: 4,
       name: "Noose",
       role: "Prisoner",
       img: "https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg?auto=compress&cs=tinysrgb&w=300",
     },
     {
+      id: 5,
       name: "Cus",
       role: "Chief",
       img: "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg?auto=compress&cs=tinysrgb&w=300",
     },
     {
+      id: 6,
       name: "Douce",
       role: "Basketball Player",
       img: "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?auto=compress&cs=tinysrgb&w=300",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        //return new employee   & setting name and role to newName and newRole
+        //'...' spread operator returns all elements of employee
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -52,10 +71,12 @@ function App() {
             {employees.map((employee) => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
